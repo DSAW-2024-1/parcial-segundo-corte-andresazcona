bcbf11fe60dfe5e367bab427947fd157899f41ac
Andres Azcona <andresazconagla@gmail.com>
2024-04-10 10:39:13 -0500

    NameFix

--- index.js
+++ index.js
@@ -31,12 +31,35 @@ app.get('/users', (req, res) => {
 
   // Aquí se simularía la obtención de la lista de usuarios de una base de datos esto se podria conectar a una base de datos noSQL como mongoDB
   let users = [
-    { nombre: 'Juan', apellido: 'Perez' },
-    { nombre: 'María', apellido: 'Gomez' },
-    { nombre: 'Pedro', apellido: 'Diaz' },
-    { nombre: 'Juan', apellido: 'Murillo' },
-    { nombre: 'Ivan', apellido: 'Duque' },
-    { nombre: 'Scott', apellido: 'Travis' },
+    { nombre: 'Samuel', apellido: 'Acero García' },
+  { nombre: 'Darek', apellido: 'Aljuri Martínez' },
+  { nombre: 'Juan Felipe', apellido: 'Cepeda Uribe' },
+  { nombre: 'Ana María', apellido: 'Chaves Pérez' },
+  { nombre: 'Carlos David', apellido: 'Cruz Pavas' },
+  { nombre: 'Diego Norberto', apellido: 'Díaz Algarín' },
+  { nombre: 'Jorge Esteban', apellido: 'Díaz Bernal' },
+  { nombre: 'David Esteban', apellido: 'Díaz Vargas' },
+  { nombre: 'Juan José', apellido: 'Forero Peña' },
+  { nombre: 'Santiago', apellido: 'Gutiérrez de Piñeres Barbosa' },
+  { nombre: 'Samuel Esteban', apellido: 'López Huertas' },
+  { nombre: 'Michael Steven', apellido: 'Medina Fernández' },
+  { nombre: 'Katherin Juliana', apellido: 'Moreno Carvajal' },
+  { nombre: 'Juan Pablo', apellido: 'Moreno Patarroyo' },
+  { nombre: 'Nicolás Esteban', apellido: 'Muñoz Sendoya' },
+  { nombre: 'Santiago', apellido: 'Navarro Cuy' },
+  { nombre: 'Juan Pablo', apellido: 'Parrado Morales' },
+  { nombre: 'Daniel Santiago', apellido: 'Ramírez Chinchilla' },
+  { nombre: 'Juan Pablo', apellido: 'Restrepo Coca' },
+  { nombre: 'Gabriela', apellido: 'Reyes González' },
+  { nombre: 'Juan José', apellido: 'Rodríguez Falla' },
+  { nombre: 'Valentina', apellido: 'Ruiz Torres' },
+  { nombre: 'Mariana', apellido: 'Salas Gutiérrez' },
+  { nombre: 'Sebastián', apellido: 'Sánchez Sandoval' },
+  { nombre: 'Josué David', apellido: 'Sarmiento Guarinzo' },
+  { nombre: 'Santiago', apellido: 'Soler Prado' },
+  { nombre: 'María Fernanda', apellido: 'Tamayo López' },
+  { nombre: 'Deivid Nicolás', apellido: 'Urrea Lara' },
+  { nombre: 'Andrés', apellido: 'Azcona' }
   ];
 // Aquí se ordena la lista de usuarios según el parámetro sort
   if (sort === 'ASC') {
